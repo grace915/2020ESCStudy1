f3fd85af47edc2419786f8f9f79b759d854228e4
LeeGaeun <grace915@naver.com>
2020-08-18 01:21:48 +0900

    weather app[mainActivity-imageView.setImageResource]

--- app/src/main/java/com/example/a2020escstudy1/MainActivity.kt
+++ app/src/main/java/com/example/a2020escstudy1/MainActivity.kt
@@ -12,38 +12,42 @@ class MainActivity : AppCompatActivity() {
 
 
         main_btn_sunny.setOnClickListener {
-            main_tv_weather.text = "맑은 날"
-            main_iv_weather.isVisible = false
-            main_iv_sunny.isVisible = true
-            main_iv_cloudy.isVisible = true
-            main_iv_rainy.isVisible = false
-            main_iv_snowy.isVisible = false
+             main_tv_weather.text = "맑은 날"
+             main_iv_weather.setImageResource(R.drawable.sunny02)
+//            main_iv_weather.isVisible = false
+//            main_iv_sunny.isVisible = true
+//            main_iv_cloudy.isVisible = true
+//            main_iv_rainy.isVisible = false
+//            main_iv_snowy.isVisible = false
 
         }
         main_btn_cloudy.setOnClickListener {
             main_tv_weather.text = "흐린 날"
-            main_iv_weather.isVisible = false
-            main_iv_sunny.isVisible = false
-            main_iv_cloudy.isVisible = true
-            main_iv_rainy.isVisible = false
-            main_iv_snowy.isVisible = false
+            main_iv_weather.setImageResource(R.drawable.cloudy02)
+  //          main_iv_weather.isVisible = false
+    //        main_iv_sunny.isVisible = false
+      //      main_iv_cloudy.isVisible = true
+        //    main_iv_rainy.isVisible = false
+          //   main_iv_snowy.isVisible = false
         }
         main_btn_rainy.setOnClickListener {
             main_tv_weather.text = "비 오는 날"
-            main_iv_weather.isVisible = false
-            main_iv_sunny.isVisible = false
-            main_iv_cloudy.isVisible = false
-            main_iv_rainy.isVisible = true
-            main_iv_snowy.isVisible = false
+            main_iv_weather.setImageResource(R.drawable.rainy01)
+  //          main_iv_weather.isVisible = false
+    //        main_iv_sunny.isVisible = false
+      //      main_iv_cloudy.isVisible = false
+        //    main_iv_rainy.isVisible = true
+          //  main_iv_snowy.isVisible = false
         }
 
         main_btn_snowy.setOnClickListener {
             main_tv_weather.text = "눈 오는 날"
-            main_iv_weather.isVisible = false
-            main_iv_sunny.isVisible = false
-            main_iv_cloudy.isVisible = false
-            main_iv_rainy.isVisible = false
-            main_iv_snowy.isVisible = true
+            main_iv_weather.setImageResource(R.drawable.snowy01)
+  //          main_iv_weather.isVisible = false
+    //        main_iv_sunny.isVisible = false
+      //      main_iv_cloudy.isVisible = false
+        //    main_iv_rainy.isVisible = false
+          //  main_iv_snowy.isVisible = true
         }
 
 
